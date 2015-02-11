600b0fe044db1426d889c174bc582e618c702a62
Les Aker <me@lesaker.org>
2015-02-10 23:50:48 -0500

    more namespacing, yay

--- lib/fidelius/zxcvbn.rb
+++ lib/fidelius/zxcvbn.rb
@@ -3,7 +3,7 @@ require 'zxcvbn'
 module Fidelius
   class Zxcvbn < Validator
     def validate(password)
-      results = Zxcvbn.test(password)
+      results = ::Zxcvbn.test(password)
       return { safe: true } if results.score == 4
       time = results.crack_time_display
       { safe: false, reason: "Password vulnerable to brute force in #{time}" }
